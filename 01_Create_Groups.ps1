dab7f24054065addbeba6cb6faf6f1a85a131a40
head-intheClouds <ningintheclouds@gmail.com>
2025-04-22 16:36:06 +0700

    Add files via upload

---
+++ 01_Create_Groups.ps1
@@ -0,0 +1,16 @@
+﻿# Create security groups
+
+$groups = @("Students", "Teachers", "Admins")
+
+foreach ($group in $groups) {
+    if (-not (Get-ADGroup -Filter "Name -eq '$group'" -ErrorAction SilentlyContinue)) {
+        New-ADGroup -Name $group `
+                    -SamAccountName $group `
+                    -GroupScope Global `
+                    -GroupCategory Security `
+                    -Path "DC=iris,DC=com,DC=vn"
+        Write-Host "Group '$group' has been created." -ForegroundColor Green
+    } else {
+        Write-Host "Group '$group' already exists. Skipping..." -ForegroundColor Yellow
+    }
+}
